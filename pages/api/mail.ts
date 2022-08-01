1aebdb905998c8e970ceae59bba0d76aab62ccdc
cauezitto <caue.matheus.granrodeo@gmail.com>
2022-07-31 23:35:52 -0300

    mudando para sendgrid

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -1,42 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import nodemailer from "nodemailer"
-import {google} from 'googleapis'
-
-const config = {
-  clientId: "6920707860-s3itafgnplgcrqsppe9d09c8200ha6vt.apps.googleusercontent.com",
-  clientSecret: "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",
-  redirectUri: "https://developers.google.com/oauthplayground" ,
-  refreshToken: "1//04tWiw4hrKPsBCgYIARAAGAQSNwF-L9IrwWc7d91tSrrmfwxm732PAlFUZ60-GMKR5crC_L6OsJeXE6MwS76h28jMytvPiNlC7Ao"
-}
-
-const oAuthClient = new google.auth.OAuth2(config.clientId, config.clientSecret, config.redirectUri)
-oAuthClient.setCredentials({refresh_token: config.refreshToken})
+import sendGrid from '@sendgrid/mail'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  sendGrid.setApiKey('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')
+const msg = {
+  to: 'stonksdevsite@gmail.com', // Change to your recipient
+  from: 'splitsendmail@gmail.com', // Change to your verified sender
+  subject: 'Novo pedido de contato!',
+  text: `${req.body.nome} ${req.body.email} ${req.body.tel} ${req.body.text}`,
+  html: `${req.body.nome} <br/> ${req.body.mail} <br/> ${req.body.tel} <br/> ${req.body.text}`,
+}
+sendGrid
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
 
-  const accessToken = await oAuthClient.getAccessToken()
-
-  let testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      type: 'OAuth2',
-      user: 'splitsendmail@gmail.com', // generated ethereal user
-      clientId: config.clientId,
-      clientSecret: config.clientSecret,
-      refreshToken: config.refreshToken,
-      accessToken: accessToken
-    }})
-
-  transporter.sendMail({
-    from: '"Site Splitconfort" <splitsendmail@gmail.com>',
-    to: "stonksdevsite@gmail.com",
-    subject: "NOVO PEDIDO DE CONTATO",
-    text: `${req.body.nome} ${req.body.tel} ${req.body.mail} ${req.body.text}`, 
-    html: `${req.body.nome} <br/> ${req.body.tel} <br/> ${req.body.mail} <br/> ${req.body.text}`,
-  });
- 
+  
   res.status(200).json({ status: 'ok' })
 }
